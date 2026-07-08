3125a3a500ce93f7d911fd8ba4b9579bd3ded540
Onkar Wakchaure <onkarwakchaure59@outlook.com>
2026-07-08 22:05:17 +0530

    Initial PySide6 application setup

---
+++ app/main.py
@@ -0,0 +1,16 @@
+import sys
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("Text-To-Speech AI Studio")
+        self.resize(1000,700)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
